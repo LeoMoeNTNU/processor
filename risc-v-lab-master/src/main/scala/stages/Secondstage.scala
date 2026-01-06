c317d7f05d42a3f5aa448d1ea4629cb51660b4f8
Leo Moe <moetheleo@gmail.com>
2026-01-06 12:19:13 +0100

    three new stages

--- risc-v-lab-master/src/main/scala/stages/Secondstage.scala
+++ risc-v-lab-master/src/main/scala/stages/Secondstage.scala
@@ -1,28 +1,30 @@
 import chisel3._
 import chisel3.util._
 
-class ID extends Module {
+class SecondStage extends Module {
 
   val io = IO(new Bundle {
     // Input: vector of enum values
     //The input should be: colorMoving, from To, newBoard.
 
     val PCIn = Input(UInt(32.W))
-    val PCOut=Output(UInt(32.W))
 
-    val instruction=Input(UInt(32.W))
+    val Instruction=Input(UInt(32.W))
+    
+    val PCOut=Output(UInt(32.W))
     val readData1=Output(UInt(32.W))
     val readData2=Output(UInt(32.W))
 
+    val IR=Output(UInt(32.W))
 
 
   })
 
-    val instruction_reg=RegInit(UInt(32.W))
-    instruction_reg:=io.instruction
+    val PCIn_reg=RegInit(UInt(32.W))
+    PCIn_reg:=io.PCIn
 
-    val PCIN_reg=RegInit(UInt(32.W))
-    PCIN_reg:=io.PCIn
-    io.PCOut:=PCIN_reg
+    val Instruction_reg=RegInit(UInt(32.W))
+    Instruction_reg:=io.Instruction
+    
 
 }
---
+++ risc-v-lab-master/src/main/scala/stages/Thirdstage.scala
@@ -0,0 +1,40 @@
+import chisel3._
+import chisel3.util._
+
+class MEM extends Module {
+
+  val io = IO(new Bundle {
+    // Input: vector of enum values
+    //The input should be: colorMoving, from To, newBoard.
+
+    val RF_input1=Input(UInt(32.W))//pretty sure that these dont need to be 32 bits. 
+    val RF_input2=Input(UInt(32.W))
+
+
+    val IR=Input(UInt(32.W))
+
+    val value1=Output(UInt(32.W))
+    val value2=Output(UInt(32.W))
+
+    val datapointer=Output(UInt(32.W))
+    val dataMemoryActive=Output(Bool())
+    val dataReadOrWrite=Output(Bool())//read is true and write is false. 
+  })
+
+  //This one needs an Instruction Memory
+
+
+    val RF_input1_reg=RegInit(UInt(32.W))
+    RF_input1_reg:=io.RF_input1
+    
+    val RF_input2_reg=RegInit(UInt(32.W))
+    RF_input2_reg:=io.RF_input2
+
+    val IR_reg=RegInit(UInt(32.W))
+    IR_reg:=io.IR
+
+
+   
+
+
+}
