9c93a2bf5ae29942a3e51b6ddff613973a45cf05
Victor <you@example.com>
2026-01-06 15:47:45 +0100

    push before i go home

--- risc-v-lab-master/src/main/scala/ALU.scala
+++ risc-v-lab-master/src/main/scala/ALU.scala
@@ -1,40 +1,40 @@
+package processor
+
 import chisel3._
 import chisel3.util._
-
 
 class ALU extends Module {  
   val io = IO(new Bundle {
     //inputs
-    val instruction = Input(UInt(32.W))
-    val funct3 = Input(UInt(3.W))
-    val funct7 = Input(UInt(7.W))
-    val imm = Input(SInt(32.W))
-    val pc = Input(UInt(32.W))
-    val opcode = Input(UInt(7.W))
+    val alu_instruction = Input(UInt(32.W))
+    val alu_funct7 = Input(UInt(7.W))
+    val alu_funct3 = Input(UInt(3.W))
+    val alu_imm = Input(SInt(32.W))
+    val alu_pc = Input(UInt(32.W))
+    val alu_opcode = Input(UInt(7.W))
 
-    val operand2 = Input(SInt(32.W))
-    val operand1 = Input(SInt(32.W))
+    val alu_op1 = Input(SInt(32.W)) // Operand 1
+    val alu_op2 = Input(SInt(32.W)) //Operand 2
 
-    val ALU_out = Output(SInt(32.W))
+    val alu_out = Output(SInt(32.W)) // ALU output
   })
 
-
-  val opcode = io.opcode
-  val funct3 = io.funct3
-  val funct7 = io.funct7
-  val operand1 = io.operand1
-  val operand2 = io.operand2
+  val alu_opcode = Utils.opcode(io.alu_instruction)
+  val alu_funct3 = Utils.funct3(io.alu_instruction)
+  val alu_funct7 = Utils.funct7(io.alu_instruction)
+  val alu_op1 = io.alu_op1
+  val alu_op2 = io.alu_op2
 
   val result = Wire(SInt(32.W))
 
   // Switch to execute correct instruction
-  switch(opcode) {
+  switch(alu_opcode) {
     is(0x33.U) { // R  types
-      switch(funct3) { //
+      switch(alu_funct3) { //
         is(0.U) {
-          switch(funct7) {
+          switch(alu_funct7) {
             is(0x0.U) {
-              io.ALU_out := operand1.asSInt + operand2.asSInt
+              io.alu_out := alu_op1.asSInt + alu_op2.asSInt
             }
           }
         }
