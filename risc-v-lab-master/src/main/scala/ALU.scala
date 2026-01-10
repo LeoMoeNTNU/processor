f75228a050f32d6f29498e8c58a19b047016e356
Leo Moe <moetheleo@gmail.com>
2026-01-10 14:09:19 +0100

    ALU now has more functions, arent tested

--- risc-v-lab-master/src/main/scala/ALU.scala
+++ risc-v-lab-master/src/main/scala/ALU.scala
@@ -11,6 +11,8 @@ class ALU extends Module {
     val from1=Input(UInt(5.W))
     val from2=Input(UInt(5.W))
 
+    val operation= Input(UInt(4.W))
+
     //val mathematical_operation =Input(UInt(x.W))
     //for now I just assume add and check that the forwarding works.
 
@@ -37,8 +39,62 @@ class ALU extends Module {
   }.otherwise{
     inputToALU2:=io.val2
   }
+  val output_inside=WireDefault(0.U(32.W))
 
-  val output_inside=inputToALU1+inputToALU2//This may cause overflow. I only want 32 bits. 
+  switch(io.operation){
+    is(0.U){
+      output_inside:= inputToALU1+inputToALU2
+    }
+    is(1.U){
+      output_inside:= inputToALU1-inputToALU2
+
+    }
+    is(2.U){
+      output_inside:= inputToALU1^inputToALU2//XOR NOT DONE
+
+    }
+    is(3.U){
+            output_inside:= inputToALU1|inputToALU2 //OR NOT DONE
+
+    }
+    is(4.U){
+      output_inside:= inputToALU1&inputToALU2 //AND NOT DONE
+
+    }
+    is(5.U){
+      output_inside:= inputToALU1<<inputToALU2
+    }
+    is(6.U){
+      output_inside:=inputToALU1>>inputToALU2
+    }
+    is(7.U){
+      output_inside:=Cat(inputToALU1(31),(inputToALU1>>inputToALU2))//I hope this works but what do I actually know. 
+    }
+    is(8.U){
+      output_inside := Mux(inputToALU1.asSInt<inputToALU2.asSInt, 1.U, 0.U)
+
+
+    }
+    is(9.U){
+      output_inside := Mux(inputToALU1<inputToALU2, 1.U, 0.U)//need to test this one and the previous one. 
+
+    }
+    is(10.U){
+      output_inside:= inputToALU1<<inputToALU2(4,0)
+    }
+    is(11.U){
+output_inside:= inputToALU1>>inputToALU2(4,0)
+    }
+    is(12.U){
+      output_inside:=Cat(inputToALU1(31),(inputToALU1>>inputToALU2(4,0)))
+    }
+
+
+
+
+  }
+
+
 
   io.output:=output_inside
   storedValue:=output_inside
